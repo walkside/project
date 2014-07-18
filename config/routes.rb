ce6c08387e41b4e713a861cecf3e195d6a459c07
root <root@pbaez.com>
2014-07-17 20:22:45 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  resources :users
+
+  root  'static_pages#home'
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+end
